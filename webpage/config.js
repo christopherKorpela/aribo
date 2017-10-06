0643785670522d9a7a0ada936c330b42320311a3
Kenneth Chaney <kpc011895@gmail.com>
2017-10-06 11:01:31 -0400

    changed to temp, alpha, gamma. Added namespace functionality. Cleaned

--- webpage/config.js
+++ webpage/config.js
@@ -1,7 +1,10 @@
 var ip='10.0.0.136'
 var web_socket_port='8080'
-var imageTopic='/cam0/image_raw'
-var gpsTopic='/gps/fix'
-var sensorList=['TEMP','CO2','CO','METHANE','GAS']
-var warningMin=[10,10,10,10,10]
-var dangerMin=[30,30,30,30,30]
+
+var sensorNamespace='quad0'
+var imageTopic='image_raw'
+var gpsTopic='fix'
+var sensorList=['TEMP','alpha','gamma']
+
+var warningMin=[10,10,10]
+var dangerMin=[30,30,30]
