f87df219cbddb6829767f361061b2d82d48ab453
Kenneth Chaney <kpc011895@gmail.com>
2017-10-06 10:28:18 -0400

    initially working google maps feed

--- webpage/config.js
+++ webpage/config.js
@@ -1,6 +1,7 @@
-var ip='192.168.200.129'
-var cmdTopic='/usma_remote/webcmd'
-var imageTopic='/usb_cam/image_raw'
+var ip='10.0.0.136'
+var web_socket_port='8080'
+var imageTopic='/cam0/image_raw'
+var gpsTopic='/gps/fix'
 var sensorList=['TEMP','CO2','CO','METHANE','GAS']
 var warningMin=[10,10,10,10,10]
 var dangerMin=[30,30,30,30,30]
