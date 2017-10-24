0fdaa7010f7c1b22baf1dfc7c4a369dbc1bd2416
Christopher Korpela <christopher.korpela@gmail.com>
2017-10-24 15:50:34 -0400

    update to config to fix image topic

--- webpage/config.js
+++ webpage/config.js
@@ -1,8 +1,8 @@
 var ip='192.168.200.88'
 var web_socket_port='8080'
 
-var sensorNamespace='quad0'
-var imageTopic='image_raw'
+var sensorNamespace='quad0'         // the namespace to apply to the connection
+var imageTopic='/usb_cam/image_raw' // if the topics aren't relative to the namespace /topic_name otherwise topic_name -> /ns/topic_name
 var gpsTopic='fix'
 var sensorList=['TEMP','alpha','gamma']
 
