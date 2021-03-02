40f3e761cb5f6c449ebc5517b77c17829ce418d2
Christopher Korpela <christopher.korpela@gmail.com>
2021-03-02 13:56:16 -0500

    major updates

--- scripts/usma_remote_interface_setup.sh
+++ scripts/usma_remote_interface_setup.sh
@@ -9,7 +9,7 @@ rospd usma_remote_interface/webpage
 sudo apt-get update
 sudo apt-get install apache2
 
-sudo apt-get install ros-indigo-rosbridge-server ros-indigo-mjpeg-server
+sudo apt-get install ros-melodic-rosbridge-server ros-melodic-web-video-server
 
 echo "
 <VirtualHost *:80>
--- webpage/config.js
+++ webpage/config.js
@@ -1,6 +1,6 @@
-var ip='129.25.56.198'
+var ip='10.212.199.189'
 var cmdTopic='/usma_remote/webcmd'
 var imageTopic='/usb_cam/image_raw'
-var sensorList=['TEMP','CO2','CO','METHANE','GAS']
-var warningMin=[10,10,10,10,10]
-var dangerMin=[30,30,30,30,30]
+var sensorList=['TEMP','GAMMA','NEUTRON']
+var warningMin=[10,10,10]
+var dangerMin=[30,30,30]
